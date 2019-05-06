50e1da492bbcbf1751d5312aca5cae2df487cfe3
ehsanatwork <ehsanatwork@gmail.com>
2019-05-06 20:06:50 +0600

    add dynamic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_title = "")
+    base_title = "A simple ruby on rails blog application"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
