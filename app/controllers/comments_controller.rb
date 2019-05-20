36793d95648416a593b1ac8075a8ca3863da75f2
ehsanatwork <ehsanatwork@gmail.com>
2019-05-21 02:35:49 +0600

    add comment with update option

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,36 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    flash[:success] = "Comment Published."
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      flash[:success] = "Comment Published."
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:success] = "Comment updated."
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:danger] = "Coment Deleted successfully"
     redirect_to post_path(@post)
   end
 
